c8067e6d288589b8971daf4f0561275e2d5f3447
Edwin <edwincas1012@gmail.com>
2026-07-19 00:31:21 -0300

    Chore: Configura Mailhog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+ Route::get('/', function () {
+    echo 'Aplicação está funcionando!';
+});
+
+
+Route::get('/email', function () {
+    Mail::raw('Teste de email', function (Message $message) {
+        $message->to('teste@example.com')
+                ->subject('Teste Email')
+                ->from('rh@example.com');
+    });
+    echo 'Email enviado com sucesso!';
 });
