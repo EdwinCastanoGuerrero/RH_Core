506e92753c7ba59fd2d235aeefaa56a0e19c2d71
Edwin <edwincas1012@gmail.com>
2026-07-23 23:22:15 -0300

    Feature: Implementa front end com componentes Blade

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,16 +41,20 @@ class FortifyServiceProvider extends ServiceProvider
             return Limit::perMinute(5)->by($throttleKey);
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
-
         RateLimiter::for('passkeys', function (Request $request) {
             $credentialId = $request->input('credential.id');
 
             return Limit::perMinute(10)->by(
                 ($credentialId ?: $request->session()->getId()).'|'.$request->ip()
             );
+        });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        Fortify::requestPasswordResetLinkView(function(){
+            return view('auth.forgot-password');
         });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,9 @@ use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
+//Middleware: encaminha para rota de login caso o usuário não esteja autenticado
+Route::middleware('auth')->group(function(){
+    Route::redirect('/', '/home');
+    Route::view('/home', 'home')->name('home');
 
- Route::get('/', function () {
-    echo 'App is running!';
-});
-
-
-Route::get('/email', function () {
-    Mail::raw('Teste de email', function (Message $message) {
-        $message->to('teste@example.com')
-                ->subject('Teste Email')
-                ->from('rh@example.com');
-    });
-    echo 'Email enviado com sucesso!';
-});
-
-Route::get('/admin', function () {
-    $admin = User::with('userDetails', 'department')->find(1);
-    return view('admin', compact('admin'));
 });
